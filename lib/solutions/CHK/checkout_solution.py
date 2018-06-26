09555824b73b2f927210f87b1923d74964f310a8
sbx_user1060 <sbx_user1060@ip-10-12-84-4.eu-west-2.compute.internal>
2018-06-26 13:46:59 +0000

    Tue Jun 26 13:46:59 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -36,26 +36,35 @@ def checkout(skus):
 
 	return cart_value
 
-def process_deal(sku, count, reg_price):
+def process_deal(sku, count, all_skus):
 	"""
 	Get the number of times a discount applies, and remainder, use that to calculate
 	the final price of that sku.
 	"""
-	# Hardcoded rules here..
+	inventory = {"A":50, "B":30, "C":20, "D":15, "E":40}
+
 	if sku == "A" and count >= 3 and count <5:
 		numdiscounts = count / 3
 		remainder = count % 3
-		return (numdiscounts * 130) + (remainder * reg_price)
+		return (numdiscounts * 130) + (remainder * inventory["A"])
 	elif sku == "A" and count >= 5:
 		numdiscounts = count / 5
 		remainder = count % 5 
-		return (numdiscounts * 200) + (remainder * reg_price)
+		return (numdiscounts * 200) + (remainder * inventory["A"])
 	elif sku == "B" and count >= 2:
 		numdiscounts = count / 2
 		remainder = count % 2
-		return (numdiscounts * 45) + (remainder * reg_price)
+		return (numdiscounts * 45) + (remainder * inventory["B"])
+	elif sku == "E":
+		if count < 2:
+			return count * reg_price
+		elif count >= 2 and "B" in all_skus:
+			# Calc how many B's to remove from final price
+			numdiscounts = count / 2
+			b_count = all_skus.count("B")
+			return (inventory["E"] * count) + ((b_count * inventory["B"]) - numdiscounts * inventory["B"])
 	else:
 		return count * reg_price
 
 
-print(process_deal("A", 6, 50))
+print(process_deal("E", 6, 50))
