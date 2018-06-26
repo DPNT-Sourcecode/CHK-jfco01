05879e7291ef8364af3e55119046c96f2a6f8050
sbx_user1060 <sbx_user1060@ip-10-12-84-4.eu-west-2.compute.internal>
2018-06-26 12:02:07 +0000

    Tue Jun 26 12:02:07 UTC 2018

---
+++ test/solution_tests/HLO/test_hlo.py
@@ -0,0 +1,24 @@
+import unittest
+
+from solutions.HLO import hello_solution
+
+
+class TestHello(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(hello_solution.hello("Jim"), "Hello, Jim!")
+
+    # def test_sum_string_params(self):
+    # 	with self.assertRaises(TypeError):
+    #         sum_solution.compute("A", '@')
+
+    # def test_sum_below_zero(self):
+    # 	with self.assertRaises(ValueError):
+    #         sum_solution.compute(-1, -2)
+
+    # def test_sum_above_hundred(self):
+    # 	with self.assertRaises(ValueError):
+    #         sum_solution.compute(101, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
