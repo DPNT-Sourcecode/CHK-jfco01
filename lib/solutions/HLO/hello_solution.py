736a59c0f6e5a2dec5803ad690e61f3fe3a27a09
sbx_user1060 <sbx_user1060@ip-10-12-84-4.eu-west-2.compute.internal>
2018-06-26 12:28:15 +0000

    Tue Jun 26 12:28:15 UTC 2018

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -5,6 +5,4 @@
 def hello(string):
 	if not isinstance(string, str):
 		raise TypeError("Parameter must be a string.")
-	if string is "":
-		raise ValueError("Name parameter cannot be blank")
 	return "Hello, {}!".format(string)
