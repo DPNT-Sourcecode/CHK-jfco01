89259d4c4bcbc10e7e83ff10528102d2cf4c7efd
sbx_user1060 <sbx_user1060@ip-10-12-84-4.eu-west-2.compute.internal>
2018-06-26 12:50:32 +0000

    Tue Jun 26 12:50:32 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,18 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+
     raise NotImplementedError()
+
+def is_deal(sku, count):
+	# Hardcoded rules here..
+	if sku == "A" and count > 3:
+		numdiscounts = count / 3
+		return numdiscounts
+	elif sku == "B" and count > 2:
+		pass
+	else:
+		return False
+
+
+print(is_deal("A", 7))
