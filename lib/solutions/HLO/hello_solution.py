f748796067bf3df6c08eebe6491850b54b71df1a
sbx_user1060 <sbx_user1060@ip-10-12-84-4.eu-west-2.compute.internal>
2018-06-26 12:31:57 +0000

    Tue Jun 26 12:31:57 UTC 2018

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,7 +2,7 @@
 
 # noinspection PyUnusedLocal
 # friend_name = unicode string
-def hello(string):
-	if not isinstance(string, str):
+def hello(friend_name):
+	if not isinstance(friend_name, str):
 		raise TypeError("Parameter must be a string.")
-	return "Hello, {}!".format(string)
+	return "Hello, {}!".format(friend_name)
