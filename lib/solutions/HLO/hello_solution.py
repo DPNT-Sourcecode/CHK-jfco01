c2bbbe18725096b1229049871f2b53032409be9f
sbx_user1060 <sbx_user1060@ip-10-12-84-4.eu-west-2.compute.internal>
2018-06-26 12:06:56 +0000

    Tue Jun 26 12:06:56 UTC 2018

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,4 +3,6 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    return "Hello, World!"
+	if not isinstance(friend_name, str):
+		raise TypeError("Parameter must be a string.")
+	return "Hello, {}!".format(friend_name)
--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -5,20 +5,11 @@ from solutions.HLO import hello_solution
 
 class TestHello(unittest.TestCase):
     def test_hello(self):
-        self.assertEqual(hello_solution.hello("Jim"), "Hello, World!")
+        self.assertEqual(hello_solution.hello("Jim"), "Hello, Jim!")
 
-    # def test_sum_string_params(self):
-    # 	with self.assertRaises(TypeError):
-    #         sum_solution.compute("A", '@')
-
-    # def test_sum_below_zero(self):
-    # 	with self.assertRaises(ValueError):
-    #         sum_solution.compute(-1, -2)
-
-    # def test_sum_above_hundred(self):
-    # 	with self.assertRaises(ValueError):
-    #         sum_solution.compute(101, 200)
-
+    def test_hello_number(self):
+    	with self.assertRaises(TypeError):
+            hello_solution.hello(1)
 
 if __name__ == '__main__':
     unittest.main()
