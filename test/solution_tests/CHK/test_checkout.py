64cc5bece1cebe0fb8689af7905772e6bfe49a5b
sbx_user1060 <sbx_user1060@ip-10-12-84-4.eu-west-2.compute.internal>
2018-06-26 12:47:32 +0000

    Tue Jun 26 12:47:32 UTC 2018

---
+++ test/solution_tests/CHK/test_checkout.py
@@ -0,0 +1,11 @@
+import unittest
+
+from solutions.CHK import checlout_solution
+
+
+class TestHello(unittest.TestCase):
+    # def test_hello(self):
+    #     self.assertEqual(hello_solution.hello("John"), "Hello, John!")
+
+if __name__ == '__main__':
+    unittest.main()
