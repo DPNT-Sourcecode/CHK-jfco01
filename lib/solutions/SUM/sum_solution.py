fb08e5011d74ae47f3bb570c297b17e6551da7e2
sbx_user1060 <sbx_user1060@ip-10-12-84-4.eu-west-2.compute.internal>
2018-06-26 11:55:22 +0000

    Tue Jun 26 11:55:22 UTC 2018

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,5 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
+	if not isinstance(x, int) or not isinstance(y, int):
+		raise TypeError("Parameters need to be of type 'INT'")
 	return x + y
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -7,6 +7,10 @@ class TestSum(unittest.TestCase):
     def test_sum(self):
         self.assertEqual(sum_solution.compute(1, 2), 3)
 
+    def test_sum_typerror(self):
+    	with self.assertRaises(TypeError):
+            sum_solution.compute("A", '@')
+
 
 if __name__ == '__main__':
     unittest.main()
