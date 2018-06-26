24189b1c5bcc8f2ef1ec05f436501668704a5c3e
sbx_user1060 <sbx_user1060@ip-10-12-84-4.eu-west-2.compute.internal>
2018-06-26 11:58:22 +0000

    Tue Jun 26 11:58:22 UTC 2018

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,4 +2,6 @@
 def compute(x, y):
 	if not isinstance(x, int) or not isinstance(y, int):
 		raise TypeError("Parameters need to be of type 'INT'")
+	if x < 0 or x > 100 or y < 0 or y > 100:
+		raise ValueError("Provided integers must be between 0 and 100")
 	return x + y
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -7,9 +7,17 @@ class TestSum(unittest.TestCase):
     def test_sum(self):
         self.assertEqual(sum_solution.compute(1, 2), 3)
 
-    def test_sum_typerror(self):
+    def test_sum_string_params(self):
     	with self.assertRaises(TypeError):
             sum_solution.compute("A", '@')
+
+    def test_sum_below_zero(self):
+    	with self.assertRaises(ValueError):
+            sum_solution.compute(-1, -2)
+
+    def test_sum_above_hundred(self):
+    	with self.assertRaises(ValueError):
+            sum_solution.compute(101, 200)
 
 
 if __name__ == '__main__':
