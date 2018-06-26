361b132a5195a51ef35e78b65181ff6e9c82432d
sbx_user1060 <sbx_user1060@ip-10-12-84-4.eu-west-2.compute.internal>
2018-06-26 12:43:46 +0000

    Tue Jun 26 12:43:46 UTC 2018

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,6 +3,4 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-	if not isinstance(friend_name, str):
-		raise TypeError("Parameter must be a string.")
 	return "Hello, {}!".format(friend_name)
--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -7,9 +7,5 @@ class TestHello(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(hello_solution.hello("John"), "Hello, John!")
 
-    def test_hello_number(self):
-        with self.assertRaises(TypeError):
-            hello_solution.hello(1)
-
 if __name__ == '__main__':
     unittest.main()
